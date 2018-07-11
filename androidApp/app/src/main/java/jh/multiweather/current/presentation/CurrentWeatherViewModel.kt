22bdd7c032558ef5025ab60637c33270df2a74e8
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-07-11 21:43:04 +0200

    Observe data from view model

--- androidApp/app/src/main/java/jh/multiweather/current/presentation/CurrentWeatherViewModel.kt
+++ androidApp/app/src/main/java/jh/multiweather/current/presentation/CurrentWeatherViewModel.kt
@@ -1,8 +1,22 @@
 package jh.multiweather.current.presentation
 
+import com.gojuno.koptional.None
+import com.gojuno.koptional.Optional
+import com.gojuno.koptional.toOptional
+import io.reactivex.Observable
+import jh.multiweather.current.model.CurrentWeatherFormatted
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CurrentWeatherViewModel @Inject constructor() {
+
+    // TODO real data
+    val currentWeatherFormattedTexts: Observable<Optional<CurrentWeatherFormatted>> = Observable.just(CurrentWeatherFormatted("26. 3. 2018", "18 °C", "Light drizzle").toOptional())
+    val currentWeatherFormattedVisibles: Observable<Boolean> = Observable.just(true)
+
+    val isLoadingVisibles: Observable<Boolean> = Observable.just(false)
+
+    val errorMessageTexts: Observable<Optional<String>> = Observable.just(None)
+    val errorMessageVisibles: Observable<Boolean> = Observable.just(false)
 }
--- androidApp/app/src/main/java/jh/multiweather/current/ui/CurrentWeatherFragment.kt
+++ androidApp/app/src/main/java/jh/multiweather/current/ui/CurrentWeatherFragment.kt
@@ -1,9 +1,12 @@
 package jh.multiweather.current.ui
 
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import jh.multiplatform.R
 import jh.multiweather.arch.ui.RxFragment
 import jh.multiweather.current.presentation.CurrentWeatherViewModel
 import jh.multiweather.main.ui.MainApplication
+import kotlinx.android.synthetic.main.current__current_weather_fragment.*
 
 class CurrentWeatherFragment : RxFragment<CurrentWeatherViewModel>() {
 
@@ -11,5 +14,29 @@ class CurrentWeatherFragment : RxFragment<CurrentWeatherViewModel>() {
 
     override fun inject() {
         MainApplication.getInjector(context).inject(this)
+    }
+
+    override fun bindViewModelToUi() = with(viewModel) {
+        listOf(
+                currentWeatherFormattedTexts.subscribe {
+                    with(it.toNullable()) {
+                        date.text = this?.date
+                        temperature.text = this?.temperatureCelsius
+                        description.text = this?.description
+                    }
+                },
+                currentWeatherFormattedVisibles.subscribe {
+                    listOf(date, temperature, description).forEach { view -> view.visibility = if (it) VISIBLE else GONE }
+                },
+                isLoadingVisibles.subscribe {
+                    progressBar.visibility = if (it) VISIBLE else GONE
+                },
+                errorMessageTexts.subscribe {
+                    errorMessage.text = it.toNullable()
+                },
+                errorMessageVisibles.subscribe {
+                    errorMessage.visibility = if (it) VISIBLE else GONE
+                }
+        )
     }
 }
