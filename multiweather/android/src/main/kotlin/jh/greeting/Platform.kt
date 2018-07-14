6c86a61d90d20af2a6fe60522bab41912649bce6
Jiří Hutárek <jiri.hutarek@cleverlance.com>
2018-07-14 13:21:01 +0200

    Remove greeting classes

--- multiweather/android/src/main/kotlin/jh/greeting/Platform.kt
+++
@@ -1,6 +0,0 @@
-package jh.greeting
-
-// TODO remove greeting classes
-internal actual class Platform actual constructor() {
-    actual val platform: String = "Android"
-}
--- multiweather/common/src/main/kotlin/jh/greeting/Platform.kt
+++
@@ -1,5 +0,0 @@
-package jh.greeting
-
-internal expect class Platform() {
-    val platform: String
-}
